f40e0c24aad6fa4ddac30dbad33a86badefafcec
Mesherekov <plof0676@gmail.com>
2025-09-09 21:13:53 +0600

    This is a new commit for what I originally planned to be amended

---
+++ app/src/main/java/com/example/noti/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.noti
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TimeInput
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.noti.ui.theme.NotiTheme
+import java.time.LocalTime
+import java.util.Calendar
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NotiTheme {
+                TimePick()
+            }
+        }
+    }
+    @Preview(showBackground = true)
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun TimePick(){
+        val currentTime = Calendar.getInstance()
+        val timePickerState = rememberTimePickerState(
+            initialHour = currentTime.get(Calendar.HOUR_OF_DAY),
+            initialMinute = currentTime.get(Calendar.MINUTE),
+            is24Hour = true,
+        )
+        Column {
+            TimeInput(
+                state = timePickerState
+            )
+        }
+    }
+
+}
+

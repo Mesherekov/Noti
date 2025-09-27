0502a0367432fdfb1d3746d0732748710b60e3ac
Mesherekov <plof0676@gmail.com>
2025-09-27 16:36:00 +0600

    Send Notification 1

--- app/src/main/java/com/example/noti/ForegroundService.kt
+++ app/src/main/java/com/example/noti/ForegroundService.kt
@@ -1,18 +1,25 @@
 package com.example.noti
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.pm.ServiceInfo
 import android.icu.util.Calendar
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
-import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 import java.util.Timer
 import java.util.TimerTask
+
 
 class ForegroundService: Service() {
     override fun onBind(p0: Intent?): IBinder? {
@@ -33,20 +40,33 @@ class ForegroundService: Service() {
         val listNoti = MainViewModel.getAllData(applicationContext).filter {
             it.isActive
         }
+//        val channel = NotificationChannel(
+//            "notes_channel",
+//            "Notes",
+//            NotificationManager.IMPORTANCE_DEFAULT
+//        )
+//        val manager = getSystemService<NotificationManager?>(NotificationManager::class.java)
+//        manager?.run {
+//            createNotificationChannel(channel)
+//        }
+
         val runCatch = runCatching {
             val timer = Timer()
             timer.schedule(
                 object : TimerTask() {
                     override fun run() {
                         val currentTime = Calendar.getInstance().time
-                        if (listNoti.any { it.hour == currentTime.hours && it.minute == currentTime.minutes }) {
-
+                        listNoti.forEach {
+                            if(it.hour == currentTime.hours && it.minute == currentTime.minutes){
+                                sendNoti(it)
+                            }
                         }
                     }
                 },
                 0,
-                10000
+                300
             )
+
         }
         runCatch.onFailure {
             Log.e("TimerError", it.toString())
@@ -70,5 +90,40 @@ class ForegroundService: Service() {
     }
     enum class Actions {
         START, STOP
+    }
+    fun sendNoti(info: NotiInfo) {
+        val time = LocalTime.of(
+            info.hour,
+            info.minute)
+        val format24hShort = time.format(DateTimeFormatter.ofPattern("HH:mm"))
+        val intent: Intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+        val noti = NotificationCompat.Builder(this, "noti_channel")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(info.message)
+            .setContentText(format24hShort)
+            .setContentIntent(pendingIntent)
+            .build()
+
+
+        val notificationManager = NotificationManagerCompat.from(applicationContext)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        notificationManager.notify(1, noti)
     }
 }
